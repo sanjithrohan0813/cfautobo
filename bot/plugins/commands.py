10adfe13fa77a0786f25c01bc05ea10f23074338
sanjithrohan0813 <84896829+sanjithrohan0813@users.noreply.github.com>
2021-05-27 12:31:19 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -76,7 +76,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'Developers', url="https://t.me/cineframesoffl"
                                 )
                         ]
                     ]
